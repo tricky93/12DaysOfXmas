73308c6ad1e1892bc3a367ed7911ce44ba795636
Patrick Walsh <patrick.walsh@thinkmoney.co.uk>
2020-12-15 18:19:38 +0000

    added first set of functions

--- christmas.go
+++ christmas.go
@@ -1,11 +1,29 @@
 package christmas
 
-gifts := map[int]string{1 : "And a partridge in a pear tree.", 2: "Two turtle doves,", 3: "Three french hens,", 4: "Four calling birds,", 5: "Five golden rings,", 6: "Six geese-a-laying,", 7:  "Six geese-a-laying,", 8: "Eight maids-a-milking,", 9: "Nine ladies dancing,", 10: "Ten lords-a-leaping,", 11: "Eleven pipers piping", 12: "Twelve drummers drumming,"}
+var gifts = map[int]string{1: "And a partridge in a pear tree.", 2: "Two turtle doves,", 3: "Three french hens,", 4: "Four calling birds,", 5: "Five golden rings,", 6: "Six geese-a-laying,", 7: "Six geese-a-laying,", 8: "Eight maids-a-milking,", 9: "Nine ladies dancing,", 10: "Ten lords-a-leaping,", 11: "Eleven pipers piping", 12: "Twelve drummers drumming,"}
 
-days := map[int]string{1: "first", 2: "second", 3: "third", 4: "fouth", 5: "fifth", 6: "sixth", 7: "seventh", 8: "eighth", 9: "ninth", 10: "tenth", 11: "eleventh", 12: "twelfth"}
+var days = map[int]string{1: "first", 2: "second", 3: "third", 4: "fouth", 5: "fifth", 6: "sixth", 7: "seventh", 8: "eighth", 9: "ninth", 10: "tenth", 11: "eleventh", 12: "twelfth"}
 
-var song [...]string
+var song []string
 
-func getGift(day int)(gift string){
-	
+func GetGift(day int) string {
+	return gifts[day]
+}
+
+func GetOrdinalNumber(day int) string {
+	return days[day]
+}
+
+func CreateOpeningLines(ordinal string) (openingLines string) {
+
+	lineOne := "On the " + ordinal + " day of Christmas,\n"
+	lineTwo := "My true love gave to me,\n"
+
+	return lineOne + lineTwo
+
+}
+
+func AppendGift(gift string) (giftList []string) {
+
+	return append(giftList, gift)
 }
--- christmas_test.go
+++ christmas_test.go
@@ -1,6 +1,71 @@
 package christmas_test
 
-import(
+import (
+	christmas "12DaysOfXmas"
 	"testing"
-	christmas ""
 )
+
+func TestGetGift(t *testing.T) {
+	t.Run("returns correct verse when passed a day", func(t *testing.T) {
+		got := christmas.GetGift(1)
+		want := "And a partridge in a pear tree."
+
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+
+		got = christmas.GetGift(10)
+		want = "Ten lords-a-leaping,"
+
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	})
+}
+
+func TestGetOrdinalNumber(t *testing.T) {
+	t.Run("returns correct ordinal number when passed a day", func(t *testing.T) {
+		got := christmas.GetOrdinalNumber(1)
+		want := "first"
+
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+
+		got = christmas.GetOrdinalNumber(10)
+		want = "tenth"
+
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	})
+}
+
+func TestCreateVerse(t *testing.T) {
+	t.Run("returns the opening line of the verse when passed a day", func(t *testing.T) {
+		got := christmas.CreateOpeningLines("first")
+		want := "On the first day of Christmas,\nMy true love gave to me,\n"
+
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+
+		got = christmas.CreateOpeningLines("fourth")
+		want = "On the fourth day of Christmas,\nMy true love gave to me,\n"
+
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	})
+}
+
+func TestAppendGift(t *testing.T) {
+	t.Run("appends the lastest gift to the global slice", func(t *testing.T) {
+		got := christmas.AppendGift("Two turtle doves,")
+		want := []string{"Two turtle doves,"}
+
+		if len(got) != len(want) {
+			t.Errorf("got %d, want %d", len(got), len(want))
+		}
+	})
+}
